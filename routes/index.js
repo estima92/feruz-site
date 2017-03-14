56d11e09c5164f19b8471ed47313369afc0d4096
Estima92 <estima1992@gmail.com>
2017-03-14 22:30:51 +0300

    sticky nav, blabla

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/services', function(req, res, next) {
+    res.render('services');
+});
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+router.get('/contacts', function(req, res, next) {
+    res.render('contacts');
+});
+
+
 module.exports = router;
